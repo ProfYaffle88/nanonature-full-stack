0f4ad359ab1a2abe3596b312ecc460f0a67a8726
James Price <james.a.t.price@gmail.com>
2024-03-15 16:56:40 +0000

    LoFi About page added.

--- plantproject/views.py
+++ plantproject/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponseRedirect
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render
+from .models import PlantProject
 
 
-class HomeView(generic.Listview):
+class HomeView(ListView):
     queryset = PlantProject.objects.all()
-    template_name = 'home.html'
+    template_name = 'plantproject/home.html'
     paginate_by = 8
