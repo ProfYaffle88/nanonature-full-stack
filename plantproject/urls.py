b604386b8311c5b12c28cc4fcbecb7408a3c8244
James Price <james.a.t.price@gmail.com>
2024-03-15 17:26:35 +0000

    Import views

--- plantproject/urls.py
+++ plantproject/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from . import views
 from .views import HomeView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('about/', views.about, name='about'),
+    path('about/', views.About, name='about'),
 ]
