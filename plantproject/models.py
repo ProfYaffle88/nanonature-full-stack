2c20aa3cebcee1a4a8df17b83b3704102a07047b
James Price <james.a.t.price@gmail.com>
2024-03-15 12:21:14 +0000

    Added PlantProject model

--- plantproject/models.py
+++ plantproject/models.py
@@ -4,3 +4,7 @@ from cloudinary.models import CloudinaryField
 class PlantProject(models.Model):
     title = models.CharField(max_length=100)
     image = CloudinaryField('image', default='placeholder')
+    about = models.TextField()
+    project_card_entries = models.ForeignKey(PlantProjectCard)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
