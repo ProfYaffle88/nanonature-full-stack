daedc67e76ef85dce099d5710e83f133eb5085e7
James Price <james.a.t.price@gmail.com>
2024-03-15 18:09:17 +0000

    Added file path for project view

--- plantproject/urls.py
+++ plantproject/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from .views import HomeView
+from .views import HomeView, ProjectView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('about/', views.About, name='about'),
+    path('project/<int:pk>', ProjectView.as_view(), name='project_view'),
 ]
