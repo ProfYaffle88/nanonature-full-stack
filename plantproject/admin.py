231241053b9a3c3fe51f17e12b8aee6d99e1079d
James Price <james.a.t.price@gmail.com>
2024-03-15 14:34:40 +0000

    Removed unnecessary comment

--- plantproject/admin.py
+++ plantproject/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import PlantProject
+from .models import PlantProject, PlantProjectCard
 
-# Register your models here.
-admin.site.register(PlantProject)
+admin.site.register(PlantProject, PlantProjectCard)
