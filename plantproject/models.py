0b1e545217853aad54abb36d72362fad0c9e0d9f
James Price <james.a.t.price@gmail.com>
2024-03-15 12:47:18 +0000

    Added docstrings to models and formatted ForeignKeys for readability

--- plantproject/models.py
+++ plantproject/models.py
@@ -3,15 +3,26 @@ from cloudinary.models import CloudinaryField
 
 
 class PlantProject(models.Model):
+    """
+    Stores a single user project, related to :model:`auth.User` and :model:`plantproject.PlantProjectCards`
+    """
+    creator = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="project_creator"
+    )
     title = models.CharField(max_length=200, unique=True)
     image = CloudinaryField('image', default='placeholder')
     about = models.TextField()
-    project_card_entries = models.ForeignKey(PlantProjectCard)
+    project_card_entries = models.ForeignKey(
+        PlantProjectCard, on_delete=models.CASCADE, related_name="plant_project_cards"
+        )
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now_add=True)
 
 
 class PlantProjectCard(models.Model):
+    """
+    Stores a single entry to a project, related to :model:`plantproject.PlantProject`
+    """
     title = models.CharField(max_length=200, unique=True)
     image = CloudinaryField('image', default='placeholder')
     entry_body = models.TextField()
