315effb85be9f34898f2d75a232435918338c047
James Price <james.a.t.price@gmail.com>
2024-03-15 14:17:18 +0000

    Restructured index.html as home.html

--- plantproject/views.py
+++ plantproject/views.py
@@ -1,6 +1,7 @@
+from django.http import HttpResponseRedirect
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'plantproject/index.html')
+def home(request):
+    return render(request, 'home.html', {})
