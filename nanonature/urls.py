4f8172a562e89089a2e5bce92643745402fa84f0
James Price <james.a.t.price@gmail.com>
2024-03-15 13:49:46 +0000

    Added url patterns for landing page and about page

--- nanonature/urls.py
+++ nanonature/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('about/', include('about.urls')),
     path('admin/', admin.site.urls),
-    path('', include('plantproject.urls')),
     path('accounts/', include('allauth.urls')),
+    path('', include('plantproject.urls')),
 ]
