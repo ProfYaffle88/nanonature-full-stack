6aabb91a46a9f023fcb0fac612853a92152763a5
James Price <james.a.t.price@gmail.com>
2024-03-15 12:26:15 +0000

    Added PlantProjectCard model and updated title fields to require to be unique

--- plantproject/models.py
+++ plantproject/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 
+
 class PlantProject(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200, unique=True)
     image = CloudinaryField('image', default='placeholder')
     about = models.TextField()
     project_card_entries = models.ForeignKey(PlantProjectCard)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now_add=True)
+
+
+class PlantProjectCard(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    image = CloudinaryField('image', default='placeholder')
+    entry_body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
