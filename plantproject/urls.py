b5d92cd9e7083187fd9946f792988d35f12b5030
James Price <james.a.t.price@gmail.com>
2024-03-18 16:03:59 +0000

    Updatedurlpatterns for ProjectCreateView

--- plantproject/urls.py
+++ plantproject/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import HomeView, ProjectView, ProjectCardView
+from .views import HomeView, ProjectView, ProjectCardView, ProjectCreateView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('about/', views.About, name='about'),
     path('project/<int:pk>', ProjectView.as_view(), name='project-view'),
     path('project/<int:project_pk>/card/<int:pk>/', ProjectCardView.as_view(), name='project-card-view'),
+    path('project/create/', ProjectCreateView.as_view(), name='project-create'),
 ]
