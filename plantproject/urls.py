cbe2f85c4521db854404aac8c5ba225c839bdea9
James Price <james.a.t.price@gmail.com>
2024-03-18 22:41:47 +0000

    registered view

--- plantproject/urls.py
+++ plantproject/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import HomeView, ProjectView, ProjectCardView, ProjectCreateView
+from .views import HomeView, ProjectView, ProjectCardView, ProjectCreateView, ProjectCardCreateView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
