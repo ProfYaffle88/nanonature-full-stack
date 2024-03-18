d8a7c61035d9156078f72ea710765aa7e10bd491
James Price <james.a.t.price@gmail.com>
2024-03-18 22:38:56 +0000

    corrected calling wrong view in url pattern

--- plantproject/urls.py
+++ plantproject/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('about/', views.About, name='about'),
     path('project/create/', ProjectCreateView.as_view(), name='project-create'),
-    path('project/<int:project_pk>/card/create/', ProjectCreateView.as_view(), name='project-card-create'),
+    path('project/<int:project_pk>/create-card/', ProjectCardCreateView.as_view(), name='project-card-create'),
     path('project/<int:project_pk>/card/<int:card_pk>/', ProjectCardView.as_view(), name='project-card-view'),
     path('project/<int:pk>/', ProjectView.as_view(), name='project-view'),
 ]
