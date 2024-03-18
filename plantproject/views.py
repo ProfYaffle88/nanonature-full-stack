d529880e79c0802fe5440bc4084870763fd0a446
James Price <james.a.t.price@gmail.com>
2024-03-18 11:12:48 +0000

    modified exisiting view and used as ProjectView. WIP

--- plantproject/views.py
+++ plantproject/views.py
@@ -21,6 +21,12 @@ class HomeView(ListView):
 class ProjectView(DetailView):
     model = PlantProject
     template_name = 'project_view.html'
+    
+    # Fetch related PlantProjectCards for the current PlantProject
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['project_cards'] = self.object.plant_project_cards.all()
+        return context
 
 
 def About(request):
