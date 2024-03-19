e2138c9d8024f25f1c20fd23770ace03cf4cf445
James Price <james.a.t.price@gmail.com>
2024-03-19 09:14:50 +0000

    Added context and corrected tag in template to get user profile projects section functioning

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,10 +1,11 @@
 app_name = 'userprofile'
 
-from django.urls import path
+from django.urls import path, include
 from . import views
 from .views import UserProfileDetailView, CustomSignupView
 
 urlpatterns = [
     path('signup/', CustomSignupView.as_view(), name='custom_signup'),
     path('profile/<int:pk>/', UserProfileDetailView.as_view(), name='user-profile'),
+    path('plantproject/', include('plantproject.urls')),
 ]
