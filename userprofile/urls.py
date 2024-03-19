05d96e75e815a8c3820620b06b27774a6b26cb22
James Price <james.a.t.price@gmail.com>
2024-03-19 01:42:10 +0000

    Created views and urls for user profile display, added to top-level urls.py

---
+++ userprofile/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+from .views import UserProfileDetailView
+
+urlpatterns = [
+    path('profile/<int:pk>/', UserProfileDetailView.as_view(), name='user-profile'),
+]
