b07e6ee8f2322c0590be299449e45dd57afed1bb
James Price <james.a.t.price@gmail.com>
2024-03-19 02:42:34 +0000

    Added new UserProfile fields to exiting custom form. Now updates both models.

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -2,8 +2,9 @@ app_name = 'userprofile'
 
 from django.urls import path
 from . import views
-from .views import UserProfileDetailView
+from .views import UserProfileDetailView, CustomSignupView
 
 urlpatterns = [
+    path('signup/', CustomSignupView.as_view(), name='custom_signup'),
     path('profile/<int:pk>/', UserProfileDetailView.as_view(), name='user-profile'),
 ]
