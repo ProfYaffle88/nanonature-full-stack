82618f1648cbb3335f73c39b1a5aa13a46ebbfae
James Price <james.a.t.price@gmail.com>
2024-03-20 18:10:18 +0000

    GITPOD: Workspace closed and would not allow back in due to billing. Recreate new workspace amd attempt to gather lost progrerss from tarball.

--- static/js/comments.js
+++ static/js/comments.js
@@ -19,11 +19,13 @@ const deleteConfirm = document.getElementById("deleteConfirm");
 */
 for (let button of editButtons) {
     button.addEventListener("click", (e) => {
-        let commentId = e.target.getAttribute("comment_id");
+        let commentId = e.target.getAttribute("data-comment_id");
         let commentContent = document.getElementById(`comment${commentId}`).innerText;
         commentText.value = commentContent;
         submitButton.innerText = "Update";
         commentForm.setAttribute("action", `edit_comment/${commentId}`);
+        commentForm.classList.add("highlighted");
+        commentContent.classList.add("highlighted");
     });
 }
 
