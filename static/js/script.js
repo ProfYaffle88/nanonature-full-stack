7c17133958801ee46a692e9f099d00daed250e56
James Price <james.a.t.price@gmail.com>
2024-03-21 17:54:26 +0000

    Attempt animated shape for header and footer

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,18 @@ function confirmDelete() {
         document.getElementById('delete-form').submit();
     }
 }
+
+
+const tween1 = KUTE.fromTo(
+    '#wave-top-1',
+    { path: '#wave-top-1' },
+    { path: '#wave-top-2' },
+    { repeat: Infinity, duration: 700, yoyo: true }
+).start();
+
+const tween2 = KUTE.fromTo(
+    '#wave-bottom-1',
+    { path: '#wave-bottom-1' },
+    { path: '#wave-bottom-2' },
+    { repeat: Infinity, duration: 700, yoyo: true }
+).start();
