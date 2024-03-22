4bcf57729fa59266442671f252d540588bc87801
James Price <james.a.t.price@gmail.com>
2024-03-22 01:33:12 +0000

    WIP: top border done, bottom border partial. Still need to fix horizontal positioning and propogate styling to pages.

--- static/js/script.js
+++ static/js/script.js
@@ -5,16 +5,16 @@ function confirmDelete() {
 }
 
 
-const tween1 = KUTE.fromTo(
+const tweentop = KUTE.fromTo(
     '#wave-top-1',
     { path: '#wave-top-1' },
     { path: '#wave-top-2' },
-    { repeat: Infinity, duration: 700, yoyo: true }
+    { repeat: Infinity, duration: 1000, yoyo: true }
 ).start();
 
-const tween2 = KUTE.fromTo(
+const tweenbottom = KUTE.fromTo(
     '#wave-bottom-1',
     { path: '#wave-bottom-1' },
     { path: '#wave-bottom-2' },
-    { repeat: Infinity, duration: 700, yoyo: true }
+    { repeat: Infinity, duration: 1000, yoyo: true }
 ).start();
