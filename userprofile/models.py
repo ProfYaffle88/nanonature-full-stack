b277283aae49191cccdff4026c54ad27f45a7591
James Price <james.a.t.price@gmail.com>
2024-03-23 22:20:06 +0000

    Fixed alreadyed registered user sign in.

--- userprofile/models.py
+++ userprofile/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from plantproject.models import PlantProject
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='userprofile')
     image = CloudinaryField('image', null=True)
     bio = models.TextField(blank=True)
     projects = models.ForeignKey(PlantProject, on_delete=models.SET_NULL, null=True, related_name='user_profiles')
